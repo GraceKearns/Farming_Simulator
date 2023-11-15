b82bcd86457a5f2b8939294a74aa68a786eb751a
Grace kearns <kearnsgracebusiness@gmail.com>
2023-11-15 17:54:37 +1100

    feat:updated

--- loadup.js
+++ loadup.js
@@ -1,15 +1,4 @@
 
-class Player {
-    constructor(x, y, moving, facing, location) {
-        this.playerX = x
-        this.playerY = y
-        this.playerWalkX = 0
-        this.playerWalkY = 0
-        this.playerMoving = moving
-        this.playerFacing = facing
-        this.playerLocation = location
-    }
-}
 var stop = false;
 var frameCount = 0;
 var fps, fpsInterval, startTime, now, then, elapsed;
@@ -21,8 +10,15 @@ function startAnimating(fps) {
 
 }
 class Game {
-
     constructor() {
+        fpsInterval = 1000 / fps;
+        then = Date.now();
+        startTime = then;
+        document.body.style.position = "absolute"
+        document.body.style.left = 0
+        document.body.style.overflowY = "hidden"
+        document.body.style.margin = 0
+        document.body.style.background = "black"
         this.imageList = [];
         this.playerlist = [];
         this.imageLoad(this.imageList)
@@ -41,28 +37,13 @@ class Game {
         console.log(this.keyPresses)
         this.ctx = this.canvas.getContext('2d')
         document.body.appendChild(this.canvas);
-        var tileSize = 20;       // The size of a tile (32×32)
-        var rowTileCount = 28;   // The number of tiles in a row of our background
-        var colTileCount = 31;   // The number of tiles in a column of our background
-        var imageNumTiles = 16;  // The number of tiles per row in the tileset image
-        fpsInterval = 1000 / fps;
-        then = Date.now();
-        startTime = then;
+     
         this.animate();
-        document.body.style.position = "absolute"
-        document.body.style.left = 0
-        document.body.style.overflowY = "hidden"
-        document.body.style.margin = 0
-        document.body.style.background = "black"
 
         window.addEventListener('keydown', (event) => {
-            console.log(event)
-            console.log(this.keyPresses)
             this.keyPresses[event.key] = true;
         }, false);
         window.addEventListener('keyup', (event) => {
-            console.log(event)
-            console.log(this.keyPresses)
             this.keyPresses[event.key] = false;
         }, false)
     }
@@ -72,43 +53,30 @@ class Game {
     }
     animate() {
         this.ctx.clearRect(0, 0, 640, 480)
-        this.drawImage(20, 26, 31, 16)
-        // calc elapsed time since last loop
+        this.drawMap(20, 26, 31, 16)
         if (this.keyPresses.w) {
-            if (this.getTile(this.playerlist[0].playerX, this.playerlist[0].playerY - 2, 20) == 1) {
-                
-            }
-            else {
+            if (!this.getTile(this.playerlist[0].playerX, this.playerlist[0].playerY - 2, 20) == 1) {
                 this.playerlist[0].playerY -= 2;
                 this.playerlist[0].playerWalkX = 0
                 this.playerlist[0].playerWalkY = 120
             }
         }
         if (this.keyPresses.a) {
-            if (this.getTile(this.playerlist[0].playerX - 2, this.playerlist[0].playerY, 20) == 1) {
-              
-            }
-            else {
+            if (!this.getTile(this.playerlist[0].playerX - 2, this.playerlist[0].playerY, 20) == 1) {
                 this.playerlist[0].playerX -= 2;
                 this.playerlist[0].playerWalkX = 0
                 this.playerlist[0].playerWalkY = 80
             }
         }
         if (this.keyPresses.s) {
-            if (this.getTile(this.playerlist[0].playerX, this.playerlist[0].playerY + 2, 20) == 1) {
-               
-            }
-            else {
+            if (!this.getTile(this.playerlist[0].playerX, this.playerlist[0].playerY + 2, 20) == 1) {
                 this.playerlist[0].playerY += 2;
                 this.playerlist[0].playerWalkX = 0
                 this.playerlist[0].playerWalkY = 0
             }
         }
         if (this.keyPresses.d) {
-            if (this.getTile(this.playerlist[0].playerX + 2, this.playerlist[0].playerY, 20) == 1) {
-                
-            }
-            else {
+            if (!this.getTile(this.playerlist[0].playerX + 2, this.playerlist[0].playerY, 20) == 1) {
                 this.playerlist[0].playerX += 2;
                 this.playerlist[0].playerWalkX = 0
                 this.playerlist[0].playerWalkY = 40
@@ -116,25 +84,12 @@ class Game {
         }
         this.drawPlayers(this.playerlist[0].playerWalkX, this.playerlist[0].playerWalkY, 40, 40, this.playerlist[0].playerX, this.playerlist[0].playerY)
         now = Date.now();
-
         elapsed = now - then;
-
-        // if enough time has elapsed, draw the next frame
-
         if (elapsed > fpsInterval) {
-
-            // Get ready for next frame by setting then=now, but also adjust for your
-            // specified fpsInterval not being a multiple of RAF's interval (16.7ms)
             then = now - (elapsed % fpsInterval);
-
-            // Put your drawing code here
-
         }
-
-
         requestAnimationFrame(() => this.animate());
     }
-
     initPlayers(playerList) {
         let player = new Player(80, 80, false, "up", 1)
         playerList.push(player)
@@ -148,10 +103,9 @@ class Game {
         imageList.push(tilesetImage2)
     }
     drawPlayers(sx, sy, width, height, x, y) {
-
         this.ctx.drawImage(this.imageList[1], sx, sy, width, height, x, y, 40, 40);
     }
-    drawImage(tileSize, rowTileCount, colTileCount, imageNumTiles) {
+    drawMap(tileSize, rowTileCount, colTileCount, imageNumTiles) {
         for (var r = 0; r < rowTileCount; r++) {
             for (var c = 0; c < colTileCount; c++) {
                 let tile = layer1[r][c];
@@ -160,7 +114,6 @@ class Game {
                 this.ctx.drawImage(this.imageList[0], (tileCol * tileSize), (tileRow * tileSize), tileSize, tileSize, (c * tileSize), (r * tileSize), tileSize, tileSize);
             }
         }
-
     }
     getTile(x, y, tileSize) {
         const tileX = Math.trunc(x / tileSize) || 0
@@ -169,15 +122,6 @@ class Game {
     }
 }
 var game = new Game();
-
-
-
-
-
-
-
-// console.log("ddd")
-// 
 
 
 
---
+++ player.js
@@ -0,0 +1,11 @@
+class Player {
+    constructor(x, y, moving, facing, location) {
+        this.playerX = x
+        this.playerY = y
+        this.playerWalkX = 0
+        this.playerWalkY = 0
+        this.playerMoving = moving
+        this.playerFacing = facing
+        this.playerLocation = location
+    }
+}
